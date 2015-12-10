ee9aa6f3b4a8370411cdb8e2cd4626c679c14f02
Katie Chang <katiechang@Pikachu-2.local>
2015-12-10 09:45:41 -0500

    evolutionchain DTO

---
+++ src/main/java/database/PokemonEvolutionChainDTO.java
@@ -0,0 +1,18 @@
+package database;
+
+/**
+ * Created by ubuntu on 12/9/15.
+ */
+public class PokemonEvolutionChainDTO {
+
+    private String Pokemon;
+
+    public String getPokemon() {
+        return Pokemon;
+    }
+
+    public void setPokemon(String pokemon) {
+        this.Pokemon = pokemon;
+    }
+
+}
